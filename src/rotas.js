4abb835bcd2753c7e68520e3c3a59639da0f455f
brsavii <brunasavi47@gmail.com>
2022-05-02 03:19:59 -0300

    Atividades 7 e 11

--- src/rotas.js
+++ src/rotas.js
@@ -2,19 +2,19 @@
     Aqui fica as rotas, guiando para menu, para a tela de calculo de IMC e para a tela do caixa eletrônico.
 */
 
-import {Route, BrowserRouter, Routes} from 'react-router-dom'
+import {Route, HashRouter, Routes} from 'react-router-dom'
 import Menu from './menu';
 import Calculadora from './components/Calculadora/Calculadora'
 import Caixa from './components/Caixa/Caixa'
 
 export default function Rotas(){
     return (
-        <BrowserRouter>
+        <HashRouter>
             <Routes>
                 <Route path='/' element={<Menu />}></Route>
                 <Route path='/calculadora' element={<Calculadora />}></Route>
                 <Route path='/caixa-saque' element={<Caixa />}></Route>
             </Routes>
-        </BrowserRouter>
+        </HashRouter>
     )
 }
